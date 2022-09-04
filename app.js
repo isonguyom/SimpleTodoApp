5bd8cb4b520397043bb9ce1eed9392530ad4718b
isonguyom <isonguyom@gmail.com>
2022-09-04 23:28:30 +0100

    filtered todo

--- app.js
+++ app.js
@@ -1,45 +1,62 @@
-const { createApp } = Vue
+const {
+    createApp
+} = Vue
 let id = 1
 
 createApp({
-  data() {
-    return {
-      pageTitle: 'Simple Todo App',
-      newTodo: "",
-      todos: [{
-            id: id++,
-            text: "Create a landing page",
-            completed: false
-          },
-          {
-            id: id++,
-            text: "Learn web 3",
-            completed: true
-          },
-          {
-            id: id++,
-            text: "Build web 3 project",
-            completed: false
-          }
-        ]
-    }
-  },
-
-  methods: {
-    createTodo() {
-        if (newTodo != "") {
-            this.todos.push({
-              id: id++,
-              text: this.newTodo,
-              completed: false
-            })          
-        } else {
-            
+    data() {
+        return {
+            pageTitle: 'Simple Todo App',
+            createTip: "Input a task to create a todo",
+            searchInput: "",
+            hideCompleted: false,
+            newTodo: "",
+            todos: [{
+                    id: id++,
+                    text: "Create a landing page",
+                    completed: false
+                },
+                {
+                    id: id++,
+                    text: "Learn web 3",
+                    completed: true
+                },
+                {
+                    id: id++,
+                    text: "Build web 3 project",
+                    completed: false
+                }
+            ]
         }
-        this.newTodo = ""
-      },
-      deleteTodo(todo) {
-        this.todos = this.todos.filter((i) => i !== todo)
-      },
-  }
+    },
+
+    computed: {
+        filteredTodo() {
+            return this.hideCompleted ?
+                this.todos
+                .filter((i) => !i.completed) :
+                this.todos
+                .filter(todo => {
+                    return todo.text.toLowerCase().includes(this.searchInput.toLowerCase())
+                })
+        }
+    },
+
+    methods: {
+        createTodo() {
+            if (newTodo != "") {
+                this.todos.push({
+                    id: id++,
+                    text: this.newTodo,
+                    completed: false
+                })
+            }
+            this.newTodo = ""
+        },
+        deleteTodo(todo) {
+            if (confirm("Are you sure you want to delete this todo")) {
+                this.todos = this.todos.filter((i) => i !== todo)
+            }
+        },
+    }
 }).mount('#app')
