a8e3c46d427b98e56598b17448b1c177410afaa1
isonguyom <isonguyom@gmail.com>
2022-09-05 00:41:25 +0100

    adjust filteredTodo method

--- app.js
+++ app.js
@@ -1,62 +1,69 @@
-const {
-    createApp
-} = Vue
+const { createApp } = Vue
 let id = 1
 
 createApp({
-    data() {
-        return {
-            pageTitle: 'Simple Todo App',
-            createTip: "Input a task to create a todo",
-            searchInput: "",
-            hideCompleted: false,
-            newTodo: "",
-            todos: [{
-                    id: id++,
-                    text: "Create a landing page",
-                    completed: false
-                },
-                {
-                    id: id++,
-                    text: "Learn web 3",
-                    completed: true
-                },
-                {
-                    id: id++,
-                    text: "Build web 3 project",
-                    completed: false
-                }
-            ]
-        }
-    },
-
-    computed: {
-        filteredTodo() {
-            return this.hideCompleted ?
-                this.todos
-                .filter((i) => !i.completed) :
-                this.todos
-                .filter(todo => {
-                    return todo.text.toLowerCase().includes(this.searchInput.toLowerCase())
-                })
-        }
-    },
-
-    methods: {
-        createTodo() {
-            if (newTodo != "") {
-                this.todos.push({
-                    id: id++,
-                    text: this.newTodo,
-                    completed: false
-                })
-            }
-            this.newTodo = ""
-        },
-        deleteTodo(todo) {
-            if (confirm("Are you sure you want to delete this todo")) {
-                this.todos = this.todos.filter((i) => i !== todo)
-            }
-        },
+  data() {
+    return {
+      pageTitle: 'Simple Todo App',
+      createTip: "Input a task to create a todo",
+      searchInput: "",
+      hideCompleted: false,
+      toggleCompletedText: "Hide completed",
+      toggleCompletedTitle: "Hide completed task",
+      newTodo: "",
+      todos: [{
+            id: id++,
+            text: "Create a landing page",
+            completed: false
+          },
+          {
+            id: id++,
+            text: "Learn web 3",
+            completed: true
+          },
+          {
+            id: id++,
+            text: "Build web 3 project",
+            completed: false
+          }
+        ]
     }
+  },
+
+  computed: {
+    searchTodo() {
+        return this.todos.filter(todo => {
+            return todo.text.toLowerCase().includes(this.searchInput.toLowerCase())
+          })
+      },
+
+      filteredTodo() {
+        return this.hideCompleted ?
+          this.searchTodo.filter((i) => !i.completed) :
+          this.searchTodo
+      }
+  },
+
+  methods: {
+    createTodo() {
+        if (newTodo != "") {  
+            this.todos.push({
+              id: id++,
+              text: this.newTodo,
+              completed: false
+            })
+        }
+        this.newTodo = ""
+      },
+      deleteTodo(todo) {
+        if (confirm("Are you sure you want to delete this todo")) {  
+            this.todos = this.todos.filter((i) => i !== todo)
+        }
+      },
+      toggleCompleted() {
+        this.hideCompleted = !this.hideCompleted;
+        this.toggleCompletedText = this.hideCompleted ? 'Show completed' : 'Hide completed';
+        this.toggleCompletedTitle = this.hideCompleted ? 'Show completed task' : 'Hide completed task';
+      }
+  }
 }).mount('#app')
